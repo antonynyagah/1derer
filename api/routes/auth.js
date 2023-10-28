090e8b56482e2084c6f9ea32d472fe66839447e1
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2023-10-27 20:20:13 -0400

    register endpoint created

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,8 @@
 import express from "express"
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req,res) =>{
-
-    res.send("Hello, this is the auth endpoint")
-})
-
-
-router.get("/register", (req,res) =>{
-
-    res.send("Hello, this is the register endpoint")
-})
+router.post("/register", register)
 
 export default router
