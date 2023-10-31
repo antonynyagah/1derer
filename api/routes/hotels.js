e511969189424f97ebeb981e61deb4ae2d214602
Antony Nyagah <antonynyagah@Antonys-MBP.lan>
2023-10-31 00:27:22 -0400

    set up room router

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,22 +1,24 @@
 import express from "express"
 import { createHotel,deleteHotel,getHotel,getHotels,updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifytoken.js"
 
 
 const router = express.Router();
 
 //create
 
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 
 //update
 
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 
 
 //delete
 
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 
 
 //get hotel by id
